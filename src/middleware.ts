906bb8b99aa3af0c149b023f7413af5fa75292e6
Wadson Pontes <wadsonpontesdesouza@hotmail.com>
2024-11-28 15:41:01 -0300

    Rota matando imagens

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,18 @@
 import { NextRequest, NextResponse } from "next/server"
 import AuthService from "./services/auth-service"
 
-export const config = { matcher: '/((?!_next/static|_next/image|favicon.ico).*)',};
+export const config = {
+    matcher: [
+      /*
+       * Match all request paths except for the ones starting with:
+       * - api (API routes)
+       * - _next/static (static files)
+       * - _next/image (image optimization files)
+       * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+       */
+      '/((?!api|_next/static|_next/image|favicon.ico|logo.png|sitemap.xml|robots.txt).*)',
+    ],
+  }
 
 const publicRoutes = ['/signup', '/login']
 const privateRoutes = ['/kanban', '/api/logout']
