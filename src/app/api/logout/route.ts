3b7d923446b903c640d407077a58f7707b175ac7
Wadson Pontes <wadsonpontesdesouza@hotmail.com>
2024-11-28 14:55:44 -0300

    Correção

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,7 +1,7 @@
 import AuthService from "@/services/auth-service"
-import { NextResponse } from "next/server"
+import { NextResponse, NextRequest } from "next/server"
 
-export async function GET(req: NextResponse) {
+export async function GET(req: NextRequest) {
     await AuthService.destroySession()
 
     return NextResponse.redirect(new URL('/login', req.url))
