f77d3e8ca1b3725aaf608ef9cb7414208a2de2ba
Wadson Pontes <wadsonpontesdesouza@hotmail.com>
2024-11-28 11:46:57 -0300

    Refatoração

--- src/components/CardWithFormSignUp.tsx
+++ src/components/CardWithFormSignUp.tsx
@@ -13,7 +13,7 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import AuthActions from "../actions/auth"
+import AuthActions from "../actions/auth-actions"
 
 export function CardWithFormSignUp() {
   return (
