852339b0778a0f648a05b2b33ae0d0a7a3a0e99d
Wadson Pontes <wadsonpontesdesouza@hotmail.com>
2024-11-29 11:40:15 -0300

    Sistema de arrastar e soltar concluido

--- src/app/kanban/page.tsx
+++ src/app/kanban/page.tsx
@@ -1,23 +1,119 @@
+'use client'
+
 import Image from "next/image"
 import { MdLogout } from "react-icons/md"
 import { GiInjustice } from "react-icons/gi"
 import { IoSearch } from "react-icons/io5"
 import { IoMdTime } from "react-icons/io"
 import { FaRegCalendar } from "react-icons/fa"
-import Link from "next/link";
-
-const columns = [
-  { title: 'Nova Publicação', cards: [{ id: 1, text: '5018120-21.2021.8.13.0022', time: '3h', date: '21/10/2024' }] },
-  { title: 'Publicação Lida', cards: [{ id: 2, text: '5018120-21.2021.8.13.0022', time: '30s', date: '21/10/2024' }] },
-  { title: 'Enviar para Advogado Responsável', cards: [] },
-  { title: 'Concluído', cards: [
-    { id: 3, text: '5018120-21.2021.8.13.0022', time: '3h', date: '21/12/2023' },
-    { id: 4, text: '5018120-21.2021.8.13.0022', time: '30d', date: '21/12/2023' },
-    { id: 5, text: '5018120-21.2021.8.13.0022', time: '71d', date: '21/12/2023' },
-  ] },
-];
+import Link from "next/link"
+import { useState } from "react"
+import { DragDropContext, Draggable, Droppable } from "@hello-pangea/dnd"
 
 export default function KanbanPage() {
+  const [dados, setDados] = useState([
+    {
+      id: "0",
+      titulo: 'Nova Publicação',
+      cards: [
+        {
+          id: "0",
+          numero: "5018120-21.2021.8.13.0022",
+          hora: '3h',
+          data: '21/10/2024',
+        },
+        {
+          id: "1",
+          numero: "Numero do processo",
+          hora: '30s',
+          data: '21/10/2024',
+        },
+        {
+          id: "2",
+          numero: "Numero desconhecido",
+          hora: '30d',
+          data: '21/10/2024',
+        },
+      ],
+    },
+    {
+      id: "1",
+      titulo: 'Publicação Lida',
+      cards: [
+        {
+          id: "3",
+          numero: "5018120-21.2021.8.13.0022",
+          hora: '3h',
+          data: '21/10/2024',
+        },
+        {
+          id: "4",
+          numero: "Numero do processo",
+          hora: '30s',
+          data: '21/10/2024',
+        },
+        {
+          id: "5",
+          numero: "Numero desconhecido",
+          hora: '30d',
+          data: '21/10/2024',
+        },
+      ],
+    },
+    {
+      id: "2",
+      titulo: 'Enviar para Advogado Responsável',
+      cards: [
+      ],
+    },
+    {
+      id: "3",
+      titulo: 'Concluído',
+      cards: [
+        {
+          id: "6",
+          numero: "5018120-21.2021.8.13.0022",
+          hora: '3h',
+          data: '21/10/2024',
+        },
+        {
+          id: "7",
+          numero: "Numero do processo",
+          hora: '30s',
+          data: '21/10/2024',
+        },
+        {
+          id: "8",
+          numero: "Numero desconhecido",
+          hora: '30d',
+          data: '21/10/2024',
+        },
+      ],
+    },
+  ])
+
+  async function onDragEnd(result: any) {
+    const { source, destination } = result;
+
+    if (destination) {
+      const sInd = +source.droppableId;
+      const dInd = +destination.droppableId;
+      const items = Array.from(dados)
+
+      if (sInd === dInd) {
+        const [target] = items[sInd].cards.splice(source.index, 1)
+        items[sInd].cards.splice(destination.index, 0, target)
+  
+        setDados(items)
+      } else {
+        const [target] = items[sInd].cards.splice(source.index, 1)
+        items[dInd].cards.splice(destination.index, 0, target)
+
+        setDados(items)
+      }
+    }
+  }
+
   return (
     <div className="min-h-screen bg-gray-100">
       <header className="flex items-center justify-between p-4 bg-white shadow-md">
@@ -67,23 +163,34 @@ export default function KanbanPage() {
         </div>
 
         <div className="grid grid-cols-4 gap-4">
-          {columns.map((col, index) => (
-            <div key={index} className="bg-white shadow rounded p-4">
-              <h2 className="mb-4 text-lg font-bold">{col.title}</h2>
-              <div className="space-y-4">
-                {col.cards.length === 0 && <p className="text-gray-400">Nenhum card encontrado</p>}
-                {col.cards.map(card => (
-                  <div key={card.id} className="p-4 bg-gray-50 border rounded shadow-sm">
-                    <p>{card.text}</p>
-                    <div className="flex items-center justify-between text-sm text-gray-500">
-                      <span className="flex"><IoMdTime size={20} style={{ marginRight: '5px' }} />{card.time}</span>
-                      <span className="flex"><FaRegCalendar size={18} style={{ marginRight: '5px' }} />{card.date}</span>
+            <DragDropContext onDragEnd={onDragEnd}>
+              {dados.map((coluna, index) => (
+                <Droppable key={index} droppableId={`${index}`} type="list" direction="vertical">
+                  {(provided) => (
+                    <div className="bg-white shadow rounded p-4" style={{ minHeight: '72vh' }} ref={provided.innerRef} {...provided.droppableProps}>
+                      <h2 className="mb-4 text-lg font-bold">{coluna.titulo}</h2>
+                      <div className="space-y-4">
+                        {coluna.cards.length === 0 && <p className="text-gray-400">Nenhum card encontrado</p>}
+                        {coluna.cards.map((card, i) => (
+                          <Draggable key={card.id} draggableId={card.id} index={i}>
+                            {(provided) => (
+                              <div className="p-4 bg-gray-50 border rounded shadow-sm" ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                                <p>{card.numero}</p>
+                                <div className="flex items-center justify-between text-sm text-gray-500">
+                                  <span className="flex"><IoMdTime size={20} style={{ marginRight: '5px' }} />{card.hora}</span>
+                                  <span className="flex"><FaRegCalendar size={18} style={{ marginRight: '5px' }} />{card.data}</span>
+                                </div>
+                              </div>
+                            )}
+                          </Draggable>
+                        ))}
+                      </div>
+                      {provided.placeholder}
                     </div>
-                  </div>
-                ))}
-              </div>
-            </div>
-          ))}
+                  )}
+                </Droppable>
+              ))}
+            </DragDropContext>
         </div>
       </div>
     </div>
