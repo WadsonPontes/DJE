74c8938c4d5573febd41ff8f648e49fea04382eb
Wadson Pontes <wadsonpontesdesouza@hotmail.com>
2024-11-29 23:17:08 -0300

    Removido não usado

--- src/app/api/kanban/route.ts
+++ src/app/api/kanban/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse, NextRequest } from "next/server"
+import { NextResponse } from "next/server"
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
